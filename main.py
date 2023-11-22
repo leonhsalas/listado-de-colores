990b66a929abbdac875653b585fcbba3edef4629
Leon Nicolhas <leonnhsalas@gmail.com>
2023-11-22 13:42:41 -0600

    segundo commit

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-lista_de_colores = []
+lista_de_colores = ["Rojo", "Azul", "Verde", "Amarillo"]
+
+lista_de_colores.append("Rosa")
+
+lista_de_colores.reverse()
+
+lista_de_colores.append("Gris")
+
+lista_de_colores.reverse()
+
+lista_de_colores.remove("Rosa")
+
+lista_de_colores.remove("Amarillo")
+
+lista_de_colores.remove("Verde")
+
+lista_de_colores.append("Naranja")
+
+lista_de_colores.append("Verde")
+
+lista_de_colores.append("Amarillo")
+
+lista_de_colores.append("Rosa")
+print(lista_de_colores)
